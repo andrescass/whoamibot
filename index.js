a3d5b0dad7ca27ec2dadcfef3f9dfd199b1e206d
U-C-2963\Cassagnes Andres <Cassagnes Andres@C-2963.invap.com.ar>
2021-01-26 18:25:27 -0300

    el bot responde

--- index.js
+++ index.js
@@ -17,3 +17,42 @@ bot.onText(/\/start/, (msg) => {
     bot.sendMessage(msg.chat.id, "Welcome");
         
     });
+
+
+bot.onText(/\/startGuessing/, (msg) => {
+
+    sentMasg = bot.sendMessage(msg.chat.id, "Dale, copate", {
+        reply_markup: {
+            inline_keyboard: [
+                [{
+                    text: "Me prendo",
+                    callback_data: JSON.stringify({
+                        'command': 'join',
+                    })
+
+                }]
+            ],
+            }
+        }).then((resutl)=>{setTimeout(() => {bot.deleteMessage(msg.chat.id, resutl.message_id);}, 60000)});
+
+    
+        
+    });
+
+join_message = ''
+
+bot.on('callback_query', function onCallbackQuery(callbackQuery) {
+    const data = JSON.parse(callbackQuery.data);
+    const opts = {
+        chat_id: callbackQuery.message.chat.id,
+        join_message: callbackQuery.message.message_id,
+        sender_id: callbackQuery.from.id,
+        sender_name: callbackQuery.from.first_name
+    };
+    if (data.command === 'join') {
+        bot.sendMessage(opts.sender_id, "Gracias por unirte " + opts.sender_name);
+    }
+});
+
+
+
